7b045f9f81556b33130b9f9ed53f6cc1aedf2713
NOCDEPT <angeloperia3@gmail.com>
2025-01-23 11:36:26 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\v1\GoogleController;
 use App\Http\Controllers\v1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,8 +13,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/authenticating', [AuthController::class, 'authenticating']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOTP']);
 Route::get('/checking-status-otp', [AuthController::class, 'checkingStatusOTP']);
-
-Route::get('/auth/redirect', [GoogleController::class, 'redirect']);
-Route::get('/auth/callback', [GoogleController::class, 'callback']);
 
 Route::get('/user-management', [UserController::class, 'index']);
